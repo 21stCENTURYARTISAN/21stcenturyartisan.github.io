31c39dc530a7e7ec946a13dfea0383e587865961
Nadav Hecht Drayson <nadavhd@gmail.com>
2026-07-25 12:57:27 +0300

    Activate EmailJS beta welcome email

--- js/beta-email-config.js
+++ js/beta-email-config.js
@@ -1,8 +1,8 @@
 // EmailJS's public browser identifiers are intended to be visible client-side.
-// Fill these three values from the EmailJS dashboard to activate the automatic
-// phone/tablet welcome email. Do not put an EmailJS private key in this file.
+// These browser identifiers activate the automatic phone/tablet welcome email.
+// Do not put an EmailJS private key in this file.
 window.GUR_EMAILJS = Object.freeze({
-  serviceId: 'YOUR_SERVICE_ID',
-  templateId: 'YOUR_TEMPLATE_ID',
-  publicKey: 'YOUR_PUBLIC_KEY'
+  serviceId: 'service_dz6p93o',
+  templateId: 'template_1r16bab',
+  publicKey: '4zjcDSkZwE0aHzaC2'
 });
